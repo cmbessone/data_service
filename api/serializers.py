5801910e316bb5866ba97f747bc5de63b83162ce
Cristian Martin Bessone <cristianbessone@gmail.com>
2024-06-12 01:32:01 -0300

    (env0) cristianbessone@MBPdeCristian3 myproject % python manage.py makemigrations Migrations for 'base':   base/migrations/0002_file.py     - Create model File (env0) cristianbessone@MBPdeCristian3 myproject % python manage.py migrate Operations to perform:   Apply all migrations: admin, auth, base, contenttypes, sessions Running migrations:   Applying base.0002_file... OK

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from base.models import Item
+from base.models import Item, File
 
 class ItemSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Item
 		fields = '__all__'
+
+
+class FileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = File
+        fields = ('id', 'file', 'uploaded_at') #Probar con all 
--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from base.models import Item
-from .serializers import ItemSerializer
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.parsers import MultiPartParser, FormParser
+from base.models import Item, File
+from .serializers import ItemSerializer, FileSerializer
 
 @api_view(['GET'])
 def getData(request):
@@ -15,3 +16,14 @@ def addItem(request):
 	if serializer.is_valid():
 		serializer.save()
 	return Response(serializer.data)
+
+
+@api_view(['POST'])
+@parser_classes([MultiPartParser, FormParser])
+def file_upload_view(request):
+    if request.method == 'POST':
+        file_serializer = FileSerializer(data=request.data)
+        if file_serializer.is_valid():
+            file_serializer.save()
+            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
